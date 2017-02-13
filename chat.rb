bf4e5a298803cd16484104eae87083da40cfe8ca
Михаил <derpiranha@gmail.com>
2017-02-14 00:04:20 +0300

    Change tabs to spaces

--- chat.rb
+++ chat.rb
@@ -4,166 +4,166 @@ require './client_settings.rb'
 
 class Chat < Qt::Widget
 
-		slots 'messend()',
-					'connection()',
-					'about()',
-					'disconnection()',
-					'clientSettings()',
-					'serverSettings()'
+    slots 'messend()',
+          'connection()',
+          'about()',
+          'disconnection()',
+          'clientSettings()',
+          'serverSettings()'
 
-		attr_reader :message 
+    attr_reader :message 
 
-		def initialize()
-			super
+    def initialize()
+      super
 
-			@clientSettingsDialog = ClientSettings.new
-			@clientSettingsDialog.getChat(self)
-			setUserName()
+      @clientSettingsDialog = ClientSettings.new
+      @clientSettingsDialog.getChat(self)
+      setUserName()
 
-			@smallEditor = Qt::TextEdit.new
-			@smallEditor.setReadOnly(true)
-			@smallEditor.setPlainText("Начало работы\n==========")
+      @smallEditor = Qt::TextEdit.new
+      @smallEditor.setReadOnly(true)
+      @smallEditor.setPlainText("Начало работы\n==========")
 
-			@mesLine = Qt::TextEdit.new
-			#@mesLine.setPlainText("Сообщение.".force_encoding('UTF-8'))
-			@mesLine.setMaximumHeight(75)
+      @mesLine = Qt::TextEdit.new
+      #@mesLine.setPlainText("Сообщение.".force_encoding('UTF-8'))
+      @mesLine.setMaximumHeight(75)
 
-			createMenu
+      createMenu
 
-			@sendButton = Qt::PushButton.new('Отправить')
+      @sendButton = Qt::PushButton.new('Отправить')
 
-	    @buttonLayout = Qt::HBoxLayout.new do |b|
-				b.addStretch(1)
-				b.addWidget(@mesLine)
-				b.addWidget(@sendButton)
-			end
-			self.setFixedSize(350, 300)
-
-
-			lllayout = Qt::VBoxLayout.new do |m|
-				@layout = Qt::VBoxLayout.new do |n|
-					n.addWidget(@smallEditor)
-					n.addLayout(@buttonLayout)
-				end
-				m.addLayout(@layout)
-				m.menuBar = @menuBar
-			end
+      @buttonLayout = Qt::HBoxLayout.new do |b|
+        b.addStretch(1)
+        b.addWidget(@mesLine)
+        b.addWidget(@sendButton)
+      end
+      self.setFixedSize(350, 300)
 
 
-			setLayout(lllayout)
-			connect(@sendButton, SIGNAL('clicked()'), self, SLOT('messend()'))
+      lllayout = Qt::VBoxLayout.new do |m|
+        @layout = Qt::VBoxLayout.new do |n|
+          n.addWidget(@smallEditor)
+          n.addLayout(@buttonLayout)
+        end
+        m.addLayout(@layout)
+        m.menuBar = @menuBar
+      end
 
-			setWindowTitle("Chat")
 
-		end
+      setLayout(lllayout)
+      connect(@sendButton, SIGNAL('clicked()'), self, SLOT('messend()'))
 
-		def set_networking(networking)
-			@net = networking
-		end
+      setWindowTitle("Chat")
 
-		def getNewSettings
-			@userName = @clientSettingsDialog.username
-		end
+    end
 
-		def clientSettings()
-			@clientSettingsDialog.startView
-		end 
+    def set_networking(networking)
+      @net = networking
+    end
 
-		def setUserName()
-			@userName = @clientSettingsDialog.username
-		end
+    def getNewSettings
+      @userName = @clientSettingsDialog.username
+    end
 
-		def disconnection()
-			begin
-				@net.sockClose
+    def clientSettings()
+      @clientSettingsDialog.startView
+    end 
 
-			rescue => err
-				puts "Ошибка #{err}"
-				Qt.execute_in_main_thread {@smallEditor.append("<b>Ошибка отключения:</b> #{err}".force_encoding('UTF-8'))}
-			end
-		end
+    def setUserName()
+      @userName = @clientSettingsDialog.username
+    end
 
-		def connection()
-			begin
-				@net.connect
+    def disconnection()
+      begin
+        @net.sockClose
 
-			rescue => err
-				#puts "Ошибка #{err}"
-				#Qt::MessageBox.about(self,"Ошибка!!!", "#{err}")
-				Qt.execute_in_main_thread {@smallEditor.append("<b>Ошибка подключения:</b> #{err}".force_encoding('UTF-8'))}
-			end
-		end
+      rescue => err
+        puts "Ошибка #{err}"
+        Qt.execute_in_main_thread {@smallEditor.append("<b>Ошибка отключения:</b> #{err}".force_encoding('UTF-8'))}
+      end
+    end
 
-		def about()
-			test = "Вот такой вот чатик.\nПока он работает плохо, но я стараюь это исправить.\nВерсия от 10.02.2017 13:52."
-		    Qt::MessageBox.about(self,"О программе",test)
-		end
+    def connection()
+      begin
+        @net.connect
 
-		def createMenu
+      rescue => err
+        #puts "Ошибка #{err}"
+        #Qt::MessageBox.about(self,"Ошибка!!!", "#{err}")
+        Qt.execute_in_main_thread {@smallEditor.append("<b>Ошибка подключения:</b> #{err}".force_encoding('UTF-8'))}
+      end
+    end
 
-	    @menuBar = Qt::MenuBar.new
+    def about()
+      test = "Вот такой вот чатик.\nПока он работает плохо, но я стараюь это исправить.\nВерсия от 10.02.2017 13:52."
+        Qt::MessageBox.about(self,"О программе",test)
+    end
 
-	    @fileMenu = Qt::Menu.new("&Файл".force_encoding('UTF-8'), self)
-			@helpMenu = Qt::Menu.new("&Помощь".force_encoding('UTF-8'), self)
-			@setMenu = Qt::Menu.new("&Настройки".force_encoding('UTF-8'), self)
+    def createMenu
 
-			@connectAct = Qt::Action.new("Подключиться".force_encoding('UTF-8'), self)
-			connect(@connectAct, SIGNAL('triggered()'), self, SLOT('connection()'))
+      @menuBar = Qt::MenuBar.new
 
-			@disconnectAct = Qt::Action.new("Отключиться".force_encoding('UTF-8'), self)
-			connect(@disconnectAct, SIGNAL('triggered()'), self, SLOT('disconnection()'))
+      @fileMenu = Qt::Menu.new("&Файл".force_encoding('UTF-8'), self)
+      @helpMenu = Qt::Menu.new("&Помощь".force_encoding('UTF-8'), self)
+      @setMenu = Qt::Menu.new("&Настройки".force_encoding('UTF-8'), self)
 
-			@exitAct = Qt::Action.new("Закрыть".force_encoding('UTF-8'), self)
-	    @exitAct.shortcut = Qt::KeySequence.new( tr("Ctrl+Q") )
-			connect(@exitAct, SIGNAL('triggered()'), self, SLOT('close()'))
+      @connectAct = Qt::Action.new("Подключиться".force_encoding('UTF-8'), self)
+      connect(@connectAct, SIGNAL('triggered()'), self, SLOT('connection()'))
 
-			@aboutAct = Qt::Action.new("О программе".force_encoding('UTF-8'), self)
-	    @aboutAct.shortcut = Qt::KeySequence.new( tr("Ctrl+H") )
-	    connect(@aboutAct, SIGNAL('triggered()'), self, SLOT('about()'))
+      @disconnectAct = Qt::Action.new("Отключиться".force_encoding('UTF-8'), self)
+      connect(@disconnectAct, SIGNAL('triggered()'), self, SLOT('disconnection()'))
 
-	    @setClientAct = Qt::Action.new("Клиент".force_encoding('UTF-8'),self)
-	    connect(@setClientAct, SIGNAL('triggered()'),self,SLOT('clientSettings()'))
+      @exitAct = Qt::Action.new("Закрыть".force_encoding('UTF-8'), self)
+      @exitAct.shortcut = Qt::KeySequence.new( tr("Ctrl+Q") )
+      connect(@exitAct, SIGNAL('triggered()'), self, SLOT('close()'))
 
- 			@fileMenu.addAction(@connectAct)
- 			@fileMenu.addAction(@disconnectAct)
-	    @fileMenu.addAction(@exitAct)
+      @aboutAct = Qt::Action.new("О программе".force_encoding('UTF-8'), self)
+      @aboutAct.shortcut = Qt::KeySequence.new( tr("Ctrl+H") )
+      connect(@aboutAct, SIGNAL('triggered()'), self, SLOT('about()'))
 
-	    @setMenu.addAction(@setClientAct)
+      @setClientAct = Qt::Action.new("Клиент".force_encoding('UTF-8'),self)
+      connect(@setClientAct, SIGNAL('triggered()'),self,SLOT('clientSettings()'))
 
-	    @helpMenu.addAction(@aboutAct)
+      @fileMenu.addAction(@connectAct)
+      @fileMenu.addAction(@disconnectAct)
+      @fileMenu.addAction(@exitAct)
 
-			@menuBar.addMenu(@fileMenu)
-			@menuBar.addMenu(@setMenu)
-	    @menuBar.addMenu(@helpMenu)
+      @setMenu.addAction(@setClientAct)
 
-		end
+      @helpMenu.addAction(@aboutAct)
 
-		def addMessage(user, text)
-			Qt.execute_in_main_thread{
-				@smallEditor.append("[" + Time.now.strftime("%d/%m/%Y %H:%M")+"] " + "#{user}: " + text.to_s.force_encoding('UTF-8'))}
-		end
+      @menuBar.addMenu(@fileMenu)
+      @menuBar.addMenu(@setMenu)
+      @menuBar.addMenu(@helpMenu)
 
-		def addMessgeFserv(text)
-			Qt.execute_in_main_thread{
-				@smallEditor.append("[" + Time.now.strftime("%d/%m/%Y %H:%M")+"] " + text.to_s.force_encoding('UTF-8'))}
-		end
+    end
 
-		def getMessage
-			@message
-		end
+    def addMessage(user, text)
+      Qt.execute_in_main_thread{
+        @smallEditor.append("[" + Time.now.strftime("%d/%m/%Y %H:%M")+"] " + "#{user}: " + text.to_s.force_encoding('UTF-8'))}
+    end
 
-		def clearMessage
-			@message = nil
-		end
-		
-		def messend()
-			unless (text = @mesLine.toPlainText.chomp).to_s.empty?
-				@message = text
-				addMessage(@userName, text)
-				#@smallEditor.append("[" + Time.now.strftime("%d/%m/%Y %H:%M")+"] " + "#{@userName}: " + text.to_s)
-				@mesLine.clear
-			end
-		end
+    def addMessgeFserv(text)
+      Qt.execute_in_main_thread{
+        @smallEditor.append("[" + Time.now.strftime("%d/%m/%Y %H:%M")+"] " + text.to_s.force_encoding('UTF-8'))}
+    end
+
+    def getMessage
+      @message
+    end
+
+    def clearMessage
+      @message = nil
+    end
+    
+    def messend()
+      unless (text = @mesLine.toPlainText.chomp).to_s.empty?
+        @message = text
+        addMessage(@userName, text)
+        #@smallEditor.append("[" + Time.now.strftime("%d/%m/%Y %H:%M")+"] " + "#{@userName}: " + text.to_s)
+        @mesLine.clear
+      end
+    end
 
 
 
--- main.rb
+++ main.rb
@@ -2,61 +2,61 @@
 
 class MainProgram 
 
-	require 'Qt'
-	require 'base64'
-	require './chat.rb' 
-	require './networking.rb'
-	require './settings.rb'
+  require 'Qt'
+  require 'base64'
+  require './chat.rb' 
+  require './networking.rb'
+  require './settings.rb'
 
-	def initialize
-		@app = Qt::Application.new(ARGV)
+  def initialize
+    @app = Qt::Application.new(ARGV)
 
-		#st = Settings.new
-		#user = st.getUsername
+    #st = Settings.new
+    #user = st.getUsername
 
-		@net = Networking.new
-		@net.setMainProgram(self)
+    @net = Networking.new
+    @net.setMainProgram(self)
 
-		@ch = Chat.new
-		@ch.set_networking(@net)
-		#@ch.setUserName(user)
+    @ch = Chat.new
+    @ch.set_networking(@net)
+    #@ch.setUserName(user)
 
-		@ch.show
-		@app.exec
-		@net.sockClose
-	end
+    @ch.show
+    @app.exec
+    @net.sockClose
+  end
 
-	def is_there_mes_from_server(servMes)
-				unless servMes == nil
-					@ch.addMessgeFserv("#{servMes}")
-					@net.clearMesFromServer
-								#servMes = Base64.decode64(servMes)
-				end
-	end
+  def is_there_mes_from_server(servMes)
+        unless servMes == nil
+          @ch.addMessgeFserv("#{servMes}")
+          @net.clearMesFromServer
+                #servMes = Base64.decode64(servMes)
+        end
+  end
 
-	def is_there_mes_from_client(clientMes)
-				unless clientMes  == nil 
-					#@ch.addMessage('main.rb', "Ответ на \"#{clientMes}\"")
+  def is_there_mes_from_client(clientMes)
+        unless clientMes  == nil 
+          #@ch.addMessage('main.rb', "Ответ на \"#{clientMes}\"")
 
-					#@ch.addMessage('networking.rb', "Получен из сети: \"#{mesFromServer}\"")
+          #@ch.addMessage('networking.rb', "Получен из сети: \"#{mesFromServer}\"")
 
-					@net.sending(clientMes)
+          @net.sending(clientMes)
 
-					clientMes = nil
-					@ch.clearMessage
-				end
-	end
+          clientMes = nil
+          @ch.clearMessage
+        end
+  end
 
-	def start_chating
-		thread = Thread.new do
-			loop do
-				is_there_mes_from_server(@net.resp[:mesFromServer])
-				#servMes = @net.resp[:mesFromServer]
-				is_there_mes_from_client(@ch.message)
-				#clientMes = @ch.message 
-			end
-		end
-	end
+  def start_chating
+    thread = Thread.new do
+      loop do
+        is_there_mes_from_server(@net.resp[:mesFromServer])
+        #servMes = @net.resp[:mesFromServer]
+        is_there_mes_from_client(@ch.message)
+        #clientMes = @ch.message 
+      end
+    end
+  end
 end
 
 m = MainProgram.new
